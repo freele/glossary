1028e0fb09a8204de165c6c96ea8c71c81fc0ce4
Alexander Kremenets <kremenets0207@gmail.com>
2015-11-01 21:24:05 +0300

    Date of creation and last update

--- api/models/statements.js
+++ api/models/statements.js
@@ -8,7 +8,19 @@ var schema = new Schema({
   },
   created_at: {
     type: Date
+  },
+  updated_at: {
+    type: Date
   }
+});
+
+schema.pre('save', function(next){
+  now = new Date();
+  this.updated_at = now;
+  if ( !this.created_at ) {
+    this.created_at = now;
+  }
+  next();
 });
 
 module.exports = mongoose.model("statements", schema);
